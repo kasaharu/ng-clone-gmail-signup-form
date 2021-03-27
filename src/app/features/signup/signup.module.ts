a75a780b560c3751093480342537b986f98fac42
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-27 19:57:33 +0900

    yarn ng g c features/signup/containers/signup

--- src/app/features/signup/signup.module.ts
+++ src/app/features/signup/signup.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SignupComponent } from './containers/signup/signup.component';
 import { SignupPageComponent } from './pages/signup/signup.component';
 import { SignupRoutingModule } from './signup-routing.module';
 
 @NgModule({
-  declarations: [SignupPageComponent],
+  declarations: [SignupPageComponent, SignupComponent],
   imports: [CommonModule, SignupRoutingModule],
 })
 export class SignupModule {}
