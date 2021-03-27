87678bfc42fbbfe2bfbc3574f3e7378247e575be
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-27 20:22:06 +0900

    yarn ng g c features/signup/components/signup-form

---
+++ src/app/features/signup/components/signup-form/signup-form.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignupFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/signup/signup.module.ts
+++ src/app/features/signup/signup.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 import { SignupComponent } from './containers/signup/signup.component';
 import { SignupPageComponent } from './pages/signup/signup.component';
 import { SignupRoutingModule } from './signup-routing.module';
 
 @NgModule({
-  declarations: [SignupPageComponent, SignupComponent],
+  declarations: [SignupPageComponent, SignupComponent, SignupFormComponent],
   imports: [CommonModule, SignupRoutingModule],
 })
 export class SignupModule {}
