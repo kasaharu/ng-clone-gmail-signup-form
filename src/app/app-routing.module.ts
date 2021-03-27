80bca6a77a178a4d683e0b13a4bd2a1ccabcb463
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-27 19:47:41 +0900

    feat: signup へのルーティング設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'signup', loadChildren: () => import('./features/signup/signup.module').then((m) => m.SignupModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/features/signup/signup-routing.module.ts
+++ src/app/features/signup/signup-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: SignupComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class SignupRoutingModule { }
+export class SignupRoutingModule {}
