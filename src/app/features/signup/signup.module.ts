9ba80b3f8d3f5bef1610ad409cc3c3b126a9b384
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-27 19:29:39 +0900

    yarn ng g module features/signup --routing

---
+++ src/app/features/signup/signup.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SignupRoutingModule } from './signup-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SignupRoutingModule,
+  ],
+})
+export class SignupModule { }
