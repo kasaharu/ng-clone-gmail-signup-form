9593394cb912c210b0c83d0291708939ae3e5108
Wataru.Kasahara <kasahara1019@gmail.com>
2021-03-31 22:25:40 +0900

    feat: home へのルーティングを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'signup', loadChildren: () => import('./features/signup/signup.module').then((m) => m.SignupModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule) },
+  { path: 'signup', loadChildren: () => import('./features/signup/signup.module').then((m) => m.SignupModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
