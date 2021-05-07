272ee87fe3bcc956899483b6a09c0f7aa2e02f8d
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-07 20:06:33 +0900

    yarn ng g module features/register --routing

---
+++ src/app/features/register/register.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { RegisterRoutingModule } from './register-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegisterRoutingModule,
+  ],
+})
+export class RegisterModule { }
