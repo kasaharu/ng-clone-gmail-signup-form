a78f75390a40fad383fa1d16f30c617b3a7c9f18
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-07 20:28:09 +0900

    feat: register へのルーティング設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule) },
   { path: 'signup', loadChildren: () => import('./features/signup/signup.module').then((m) => m.SignupModule) },
+  { path: 'register', loadChildren: () => import('./features/register/register.module').then((m) => m.RegisterModule) },
 ];
 
 @NgModule({
--- src/app/features/register/register-routing.module.ts
+++ src/app/features/register/register-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: RegisterComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class RegisterRoutingModule { }
+export class RegisterRoutingModule {}
