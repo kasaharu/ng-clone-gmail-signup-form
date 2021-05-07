6b3e9ac87bdcc9cff84e9ead4f6b201a9f502856
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-07 20:08:51 +0900

    yarn ng g c features/register/pages/register

--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { RegisterComponent } from './pages/register/register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+  ],
   imports: [
     CommonModule,
     RegisterRoutingModule,
