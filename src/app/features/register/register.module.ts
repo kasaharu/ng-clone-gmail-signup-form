e53b6911a2fd69a50b1769180812e42c6a058b32
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-07 20:33:42 +0900

    yarn ng g c features/register/containers/register

--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { RegisterComponent } from './containers/register/register.component';
 import { RegisterPageComponent } from './pages/register/register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
-  declarations: [RegisterPageComponent],
+  declarations: [RegisterPageComponent, RegisterComponent],
   imports: [CommonModule, RegisterRoutingModule],
 })
 export class RegisterModule {}
