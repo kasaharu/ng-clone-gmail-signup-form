f8c7ffcf0e87e90de9fd9d85339294b030f14ae4
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-08 10:11:41 +0900

    yarn ng g c features/register/components/register-form

---
+++ src/app/features/register/components/register-form/register-form.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterComponent } from './containers/register/register.component';
 import { RegisterPageComponent } from './pages/register/register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
-  declarations: [RegisterPageComponent, RegisterComponent],
+  declarations: [RegisterPageComponent, RegisterComponent, RegisterFormComponent],
   imports: [CommonModule, RegisterRoutingModule],
 })
 export class RegisterModule {}
