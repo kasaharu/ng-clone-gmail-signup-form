1eac5c5eafad37be301141f48ffee91079ac9a93
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-08 18:25:41 +0900

    feat: register feature で ReactiveForms を使えるように module をインポート

--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterComponent } from './containers/register/register.component';
 import { RegisterPageComponent } from './pages/register/register.component';
@@ -8,6 +8,6 @@ import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
   declarations: [RegisterPageComponent, RegisterComponent, RegisterFormComponent],
-  imports: [CommonModule, RegisterRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, RegisterRoutingModule],
 })
 export class RegisterModule {}
