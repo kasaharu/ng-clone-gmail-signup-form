08e60004058d1251d85c10a877504271b4916acb
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-08 18:28:58 +0900

    yarn ng g c features/register/components/basic-information-form

---
+++ src/app/features/register/components/basic-information-form/basic-information-form.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-information-form',
+  templateUrl: './basic-information-form.component.html',
+  styleUrls: ['./basic-information-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BasicInformationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BasicInformationFormComponent } from './components/basic-information-form/basic-information-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterComponent } from './containers/register/register.component';
 import { RegisterPageComponent } from './pages/register/register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
-  declarations: [RegisterPageComponent, RegisterComponent, RegisterFormComponent],
+  declarations: [RegisterPageComponent, RegisterComponent, RegisterFormComponent, BasicInformationFormComponent],
   imports: [CommonModule, ReactiveFormsModule, RegisterRoutingModule],
 })
 export class RegisterModule {}
