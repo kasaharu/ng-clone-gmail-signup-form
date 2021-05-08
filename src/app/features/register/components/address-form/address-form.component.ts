7334f5c6495aa4295f6ea416a202f09e0eb9f3ee
Wataru.Kasahara <kasahara1019@gmail.com>
2021-05-08 18:29:57 +0900

    yarn ng g c features/register/components/address-form

---
+++ src/app/features/register/components/address-form/address-form.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-form',
+  templateUrl: './address-form.component.html',
+  styleUrls: ['./address-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddressFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/register/register.module.ts
+++ src/app/features/register/register.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddressFormComponent } from './components/address-form/address-form.component';
 import { BasicInformationFormComponent } from './components/basic-information-form/basic-information-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterComponent } from './containers/register/register.component';
@@ -8,7 +9,7 @@ import { RegisterPageComponent } from './pages/register/register.component';
 import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
-  declarations: [RegisterPageComponent, RegisterComponent, RegisterFormComponent, BasicInformationFormComponent],
+  declarations: [RegisterPageComponent, RegisterComponent, RegisterFormComponent, BasicInformationFormComponent, AddressFormComponent],
   imports: [CommonModule, ReactiveFormsModule, RegisterRoutingModule],
 })
 export class RegisterModule {}
